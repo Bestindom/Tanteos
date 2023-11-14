effe454998f7f5932bd0cb48c4b45d7b2e144cdd
Bestindom <bestindom@gmail.com>
2023-11-14 19:18:51 +0100

    Pokemons/

--- Pokemons/database.sql
+++ Pokemons/database.sql
@@ -4,41 +4,76 @@ USE `pokemons`;
 
 DROP TABLE IF EXISTS `types`;
 CREATE TABLE `types` (
+  `id_type` int NOT NULL,
   `type` varchar(50) NOT NULL,
-  PRIMARY KEY (`type`)
+  PRIMARY KEY (`id_type`)
 );
+
+DROP TABLE IF EXISTS `regions`;
+CREATE TABLE `regions` (
+  `id_region` int,
+  `region` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_region`)
+);
+
 
 DROP TABLE IF EXISTS `pokemon`;
 CREATE TABLE `pokemon` (
-  `id_pokemon` int(11) NOT NULL,
+  `id_pokemon` int auto_increment,
+  `num_pokedex` int(11) unique,
   `name` varchar(50) NOT NULL,
-  `type` varchar(50) NOT NULL,
+  `region`int,
   `image` varchar(255) NOT NULL,
   PRIMARY KEY (`id_pokemon`),
-  FOREIGN KEY (`type`) REFERENCES `types` (`type`)
+  FOREIGN KEY (`region`) REFERENCES `regions` (`id_region`)
 );
 
-INSERT INTO `types` values
-('Normal'),
-('Fire'),
-('Water'),
-('Grass'),
-('Electric'),
-('Ice'),
-('Fighting'),
-('Poison'),
-('Ground'),
-('Flying'),
-('Psychic'),
-('Bug'),
-('Rock'),
-('Ghost'),
-('Dragon'),
-('Dark'),
-('Steel'),
-('Fairy');
 
-INSERT INTO `pokemon` values
-(001, 'Bulbasaur', 'Grass', './images/001.png'),
-(002, 'Ivysaur', 'Grass', './images/002.png'),
-(003, 'Venusaur' , 'Grass', './images/003.png');
+DROP TABLE IF EXISTS `pokemon_type`;
+CREATE TABLE `pokemon_type` (
+  `id_pokemon` int,
+  `id_type1` int not null,
+  `id_type2` int,
+  PRIMARY KEY (id_pokemon, id_type1, id_type2),
+  FOREIGN KEY (`id_pokemon`) REFERENCES `pokemon` (`id_pokemon`),
+  FOREIGN KEY (`id_type1`) REFERENCES `types` (`id_type`),
+  FOREIGN KEY (`id_type2`) REFERENCES `types` (`id_type`)
+);
+	
+
+INSERT INTO `types` values
+('1', 'Normal'),
+('2', 'Fire'),
+('3', 'Water'),
+('4', 'Grass'),
+('5', 'Electric'),
+('6', 'Ice'),
+('7', 'Fighting'),
+('8', 'Poison'),
+('9', 'Ground'),
+('10', 'Flying'),
+('11', 'Psychic'),
+('12', 'Bug'),
+('13', 'Rock'),
+('14', 'Ghost'),
+('15', 'Dragon'),
+('16', 'Dark'),
+('17', 'Steel'),
+('18', 'Fairy');
+
+
+INSERT INTO regions VALUES
+('1', 'Kanto'),
+('2', 'Johto'),
+('3', 'Hoenn'),
+('4', 'Sinnoh'),
+('5', 'Teselia'),
+('6', 'Kalos'),
+('7', 'Alola'),
+('8', 'Galar'),
+('9', 'Paldea');
+
+INSERT INTO `pokemon` (num_pokedex, name, region, image) values
+(001, 'Bulbasaur', '1', './images/001.png'),
+(002, 'Ivysaur', '2', './images/002.png'),
+(003, 'Venusaur' , '3', './images/003.png');
